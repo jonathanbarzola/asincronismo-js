c9f765ebe62bb776531f0a2863312e5ae852dfbc
jonathanbarzola <jonabl2102@gmail.com>
2022-10-22 12:19:35 -0500

    challenge async & await

---
+++ src/async/challenge.js
@@ -0,0 +1,27 @@
+import fetch from "node-fetch";
+const API = "https://api.escuelajs.co/api/v1";
+
+async function fetchData(urlAPI) {
+  const response = await fetch(urlAPI);
+  const data = await response.json();
+
+  return data;
+}
+
+const anotherFn = async (urlAPI) => {
+  try {
+    const products = await fetchData(`${urlAPI}/products`);
+    const product = await fetchData(`${urlAPI}/products/${products[0].id}`);
+    const category = await fetchData(
+      `${urlAPI}/categories/${product.category.id}`
+    );
+
+    console.log(products);
+    console.log(product.title);
+    console.log(category.name);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+anotherFn(API);
